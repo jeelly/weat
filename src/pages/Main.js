efbe7eacaf0d938f730c53d71bf0335636e49ab4
jeelly <khk3159@naver.com>
2022-07-05 17:13:26 +0900

    캐릭터 입모양 버그 수정 #1

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import characterface from '../img/characterface.svg';
-
+import mouth from '../img/mouth.svg';
 
 const Main = () => {
     return (
-        <>
-            <img src={characterface} alt="smile" />
-        </>
+        <div style={{width:"300px", border:"1px solid red", backgroundColor:"blue"}}>
+            <img src={characterface} alt="smile" style={{position:"relative"}}/>
+        </div>
     );
 };
 
