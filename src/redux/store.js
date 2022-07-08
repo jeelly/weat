8522a4338f908b7df72e1e789fa68a85a1dfb924
BO_ORAM <105050618+Bo-oram@users.noreply.github.com>
2022-07-08 23:00:57 +0900

    회원가입시 유저데이터 모아서 넘길 수 있도록 리덕스에 슬라이스 추가

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postReducer from './modules/postSlice';
+import signupReducer from './modules/userSlice'
 
 const store = configureStore({
   reducer: {
     post: postReducer,
+    userSignup:signupReducer
   },
 });
 
