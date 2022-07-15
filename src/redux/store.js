6d460af2a8e7dea10263cbc9c6e7d9a187044c38
BO_ORAM <105050618+Bo-oram@users.noreply.github.com>
2022-07-15 20:19:53 +0900

    리듀서수정

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import postReducer from './modules/postSlice';
-import signupReducer from './modules/signupSlice';
-import userCheckReducer from './modules/userSlice';
-import emojiReducer from './modules/emojiSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import postReducer from "./modules/postSlice";
+import signupReducer from "./modules/signupSlice";
+import userCheckReducer from "./modules/userSlice";
+import roomMakingReducer from "./modules/roomMakingSlice";
+
 
 const store = configureStore({
   reducer: {
     post: postReducer,
-    userSignup:signupReducer,
-    userEmoji:emojiReducer,
-    loggedIn:userCheckReducer
+    userSignup: signupReducer,    
+    loggedIn: userCheckReducer,
+    roomMaking:roomMakingReducer,
   },
 });
 
