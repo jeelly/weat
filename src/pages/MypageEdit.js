733e52c468ae859ba272f31cdebee48d6aab28c8
BO_ORAM <105050618+Bo-oram@users.noreply.github.com>
2022-07-29 16:59:44 +0900

    마이페이지

---
+++ src/pages/MypageEdit.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import close from "../img/icon/close.svg";
+import { useNavigate } from "react-router-dom";
+import { Container } from "../css/GlobalStyles";
+
+const MypageEdit = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <Goback>
+        <img
+          src={close}
+          alt=""
+          onClick={() => {
+            navigate("/mypage");
+          }}
+        />
+      </Goback>
+      <Container>
+        <Outlet />
+      </Container>
+    </>
+  );
+};
+
+const Goback = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  height: 52px;
+  background-color: #000;
+  padding: 0 16px;
+`;
+
+export default MypageEdit;
